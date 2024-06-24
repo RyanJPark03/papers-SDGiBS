e551fa871b6ad49e0096c9067aee8fb839de443a
Ryan Park <ryanjpark03@gmail.com>
2024-06-24 14:44:43 -0500

    basic skeleton for SDGiBS module

--- src/SDGiBS.jl
+++ src/SDGiBS.jl
@@ -1,8 +1,19 @@
 module SDGiBS
 
-function belief_update(belief :: BlockVector{Float64}, observation :: BlockVector{Float64})
+using BlockArrays
+
+export belief_update
+function belief_update(β :: BlockVector{Float64}, observation :: BlockVector{Float64}) 
+    
+    # all_beliefs = BlockVector{Float64}(vcat([player.belief for player in players]...), [length(player.belief) for player in players])
     # TODO: Implement belief update
     return belief
+end
+
+export SDGiBS_solve_action
+function SDGiBS_solve_action(players :: Array{T1}, env :: T2, current_player_index :: Int)
+    # TODO: datatype T must have a "belief," "cost" 
+    return [1, 1]
 end
 
 end 
